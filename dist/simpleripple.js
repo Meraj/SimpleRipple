be2d1dc630fc0213603ecafd0c8ab2c06e3fc4fc
MerajV <meraj.sheikhhaa@gmail.com>
2020-12-07 19:08:24 +0330

    Update simpleripple.js

--- dist/simpleripple.js
+++ dist/simpleripple.js
@@ -1,35 +1,25 @@
 function simpleRipple(Item) {
-    const RippleClass = $(".simpleRipple-ripple")
     Item.addClass("simpleRipple")
     Item.click(function (e) {
+        $(".simpleRipple-ripple").remove();
 
-        // Remove any old one
-        RippleClass.remove();
-
-        // Setup
         var posX = $(this).offset().left,
             posY = $(this).offset().top,
             buttonWidth = $(this).width(),
             buttonHeight =  $(this).height();
 
-        // Add the element
         $(this).prepend("<span class='simpleRipple-ripple'></span>");
 
-
-        // Make it round!
         if(buttonWidth >= buttonHeight) {
             buttonHeight = buttonWidth;
         } else {
             buttonWidth = buttonHeight;
         }
 
-        // Get the center of the element
         var x = e.pageX - posX - buttonWidth / 2;
         var y = e.pageY - posY - buttonHeight / 2;
 
-
-        // Add the ripples CSS and start the animation
-        RippleClass.css({
+        $(".simpleRipple-ripple").css({
             width: buttonWidth,
             height: buttonHeight,
             top: y + 'px',
